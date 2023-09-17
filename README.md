3950bd835b05144e728398d8bbb9bc0f78acc858
jcarvallo <jcarvallo.ar@gmail.com>
2023-09-16 22:33:57 -0300

    add swagger

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ bun run index.ts
 ```
 
 This project was created using `bun init` in bun v1.0.2. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+Swagger url: http://localhost:8080/swagger
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
 
 const app = new Elysia()
+    .use(swagger())
     .use(import('./routers/user.route'))
     .listen(process?.env?.PORT || 10)
 
